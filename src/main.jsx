b6660faa79a8ae1ff0eb68506a8d911316223ee9
VasylievYurii <uraplanetianin@gmail.com>
2023-11-09 01:16:28 +0200

    basename

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx'; 
+import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react_vite">
+    <BrowserRouter basename="/PowerPulse2.0">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
