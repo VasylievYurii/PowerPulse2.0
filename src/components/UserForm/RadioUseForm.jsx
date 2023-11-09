ab1031558520c5bd9cb01123f5374abec2a78f98
KaterynaValuiska <katya.barry.ubf@gmail.com>
2023-11-09 22:34:08 +0100

    userForm

---
+++ src/components/UserForm/RadioUseForm.jsx
@@ -0,0 +1,48 @@
+import './useForm.css';
+import RadioBtn from './RadioBtn';
+export const RadioUseForm = () => {
+  return (
+    <div>
+      <p className="text-blood">Blood</p>
+      <div className="content-blood-gender">
+        <div className="content-blood">
+          <RadioBtn name="blood" value="1" text="1" />
+          <RadioBtn name="blood" value="2" text="2" />
+          <RadioBtn name="blood" value="3" text="3" />
+          <RadioBtn name="blood" value="4" text="4" />
+        </div>
+        <div className="content-gender">
+          <RadioBtn name="sex" value="male" text="Male" />
+          <RadioBtn name="sex" value="female" text="Female" />
+        </div>
+      </div>
+      <div className="lifestyle-btn">
+        <RadioBtn
+          name="levelActivity"
+          value="1"
+          text="Sedentary lifestyle (little or no physical activity)"
+        />
+        <RadioBtn
+          name="levelActivity"
+          value="2"
+          text="Light activity (light exercises/sports 1-3 days per week)"
+        />
+        <RadioBtn
+          name="levelActivity"
+          value="3"
+          text="Moderately active (moderate exercises/sports 3-5 days per week)"
+        />
+        <RadioBtn
+          name="levelActivity"
+          value="4"
+          text="Very active (intense exercises/sports 6-7 days per week)"
+        />
+        <RadioBtn
+          name="levelActivity"
+          value="5"
+          text="Extremely active (very strenuous exercises/sports and physical work)"
+        />
+      </div>
+    </div>
+  );
+};
