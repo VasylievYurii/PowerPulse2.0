42684e8fa3f9be6640f246cb5f40cfd5c7fb75ba
kika88vk <69463446+kika88vk@users.noreply.github.com>
2023-11-09 21:17:15 +0200

    Add basic form

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Formik } from 'formik';
+
+//    /^[\w-.]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
+
+const initialValues = {
+  name: '',
+  email: '',
+  password: '',
+};
 
 const SignUpForm = () => {
-  return <div></div>;
+  return (
+    <Formik initialValues={initialValues}>
+      <form autoComplete="off">
+        <label htmlFor="name">
+          Name <input type="text" name="name" />
+        </label>
+        <label htmlFor="email">
+          Email <input type="text" name="email" />
+        </label>
+        <label htmlFor="password">
+          Password <input type="text" name="password" />
+        </label>
+        <button type="submit">Sign Up</button>
+      </form>
+    </Formik>
+  );
 };
 
 export default SignUpForm;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,7 +3,12 @@ import SectionTemplate from '../components/SectionTemplate/SectionTemplate';
 const SignUp = () => {
   return (
     <SectionTemplate>
-      <p>SignUp page</p>
+      <h1>Sign Up</h1>
+      <p>
+        Thank you for your interest in our platform. To complete the
+        registration process, please provide us with the following information.
+      </p>
+      <p>Already have an account? Sign In</p>
     </SectionTemplate>
   );
 };
