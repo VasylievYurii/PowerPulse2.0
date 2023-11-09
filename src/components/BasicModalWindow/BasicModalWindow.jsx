cf6e8a3e4232680f001b62f295941a7f4b5623e0
OlgaBilynska <olgabilinskaya@gmail.com>
2023-11-09 23:31:50 +0200

    basicModalWindow

--- src/components/BasicModalWindow/BasicModalWindow.jsx
+++ src/components/BasicModalWindow/BasicModalWindow.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { Children } from 'react';
+import sprite from '../../assets/sprite.svg';
+import {
+  ModalContainer,
+  CloseBtnWrapper,
+  ModalChildrenWrapper,
+} from './BasicModalWindow.styled';
 
 const BasicModalWindow = () => {
-  return <div></div>;
+  return (
+    <ModalContainer>
+      <CloseBtnWrapper>
+        <use href={`${sprite}#icon-checkbox`} />
+      </CloseBtnWrapper>
+
+      <ModalChildrenWrapper>{Children}</ModalChildrenWrapper>
+    </ModalContainer>
+  );
 };
 
 export default BasicModalWindow;
--- src/components/BasicModalWindow/BasicModalWindow.styled.js
+++ src/components/BasicModalWindow/BasicModalWindow.styled.js
@@ -1,1 +1,18 @@
 import styled from 'styled-components';
+
+export const ModalContainer = styled.div`
+  padding: 10px;
+`;
+
+export const CloseBtnWrapper = styled.svg`
+  width: 22px;
+  height: 22px;
+  stroke: var(--color-text);
+  margin-top: 14px;
+  margin-right: 14px;
+`;
+
+export const ModalChildrenWrapper = styled.div`
+  margin: 0 auto;
+  text-align: center;
+`;
