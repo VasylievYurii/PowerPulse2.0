b8d5079af034cac105103e87f9867ce2bfe2ef56
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-11-11 01:43:45 +0200

    add text

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,18 @@
-import { Container, Title } from './ErrorPage.styled';
+import { Container, Title, Text, LinkBtn, Button } from './ErrorPage.styled';
 
 const ErrorPage = () => {
   return (
     <Container>
       <Title>404</Title>
+      <Text>Sorry, you have reached a page that we could not find. It seems that you are lost among
+        the numbers and letters of our virtual space. Perhaps this page went on vacation or
+        decided to disappear into another dimension. We apologize for this inconvenience.</Text>
+      <LinkBtn to="/diary">
+        <Button>Go Home</Button>
+      </LinkBtn>
     </Container>
   );
 };
 
 export default ErrorPage;
+
--- src/pages/ErrorPage/ErrorPage.styled.js
+++ src/pages/ErrorPage/ErrorPage.styled.js
@@ -16,3 +16,23 @@ export const Title = styled.h1`
     0 0 40px #00baff,
     0 0 80px #00baff;
 `;
+
+export const Text = styled.p`
+ color: var(--color-text);
+  font-size: 16px;
+  line-height: 1.5;
+  margin-bottom: 28px;
+`
+  ;
+
+export const LinkBtn = styled(Link)`
+border-radius: 1px;
+color: var(--color-main-three);
+  `;
+
+export const Button = styled.p`
+color: var(--color-text);
+width: 500;
+font-size: 20px;
+line-height: 1.2;
+  `;
