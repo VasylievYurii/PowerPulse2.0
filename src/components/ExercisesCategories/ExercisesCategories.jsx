79c82e83587b217817a6f788e60c35bb580b689d
shkurkoka <shkurkoka@gmail.com>
2023-11-10 17:54:07 +0200

    Create

--- src/components/ExercisesCategories/ExercisesCategories.jsx
+++ src/components/ExercisesCategories/ExercisesCategories.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 const ExercisesCategories = () => {
-  return <div></div>;
+  return (
+    <div className='ExercisesCategories_ExercisesPage'>
+      <h2>Exercices</h2>
+      <ul className="exercices_nav">
+        <li className="exercices_nav_item">
+          <NavLink to="/">Body parts</NavLink>
+        </li>
+        <li className="exercices_nav_item">
+          <NavLink to="/">Muscles</NavLink>
+        </li>
+        <li className="exercices_nav_item">
+          <NavLink to="/">Equipment</NavLink>
+        </li>
+      </ul>
+    </div>
+  ) 
 };
 
 export default ExercisesCategories;
