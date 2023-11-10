1117c108b65131c473f7df121dc432da2c93500d
OlenaIa <121794911+OlenaIa@users.noreply.github.com>
2023-11-10 12:49:45 +0200

    TitlePage

--- src/components/TitlePage/TitlePage.styled.js
+++ src/components/TitlePage/TitlePage.styled.js
@@ -1,1 +1,10 @@
 import styled from 'styled-components';
+
+const TitlePageStyle = styled.h2`
+color: var(--color-text);
+font-size: 24px;
+font-weight: 700;
+line-height: 1,17
+`;
+
+export default TitlePageStyle;
--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -1,8 +1,10 @@
 import SectionTemplate from '../components/SectionTemplate/SectionTemplate';
+import { TitlePage } from '../components/TitlePage';
 
 const Diary = () => {
   return (
     <SectionTemplate>
+      <TitlePage title='Diary'/>
       <p>Diary page</p>
     </SectionTemplate>
   );
