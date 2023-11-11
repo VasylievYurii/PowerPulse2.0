2e0d2fc38ed7b5dda30f44adfd6213c529e1abe6
shkurkoka <shkurkoka@gmail.com>
2023-11-11 10:39:07 +0200

    fix create

--- src/components/ExercisesCategories/ExercisesCategories.jsx
+++ src/components/ExercisesCategories/ExercisesCategories.jsx
@@ -1,23 +1,7 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
 
 const ExercisesCategories = () => {
-  return (
-    <div className='ExercisesCategories_ExercisesPage'>
-      <h2>Exercices</h2>
-      <ul className="exercices_nav">
-        <li className="exercices_nav_item">
-          <NavLink to="/">Body parts</NavLink>
-        </li>
-        <li className="exercices_nav_item">
-          <NavLink to="/">Muscles</NavLink>
-        </li>
-        <li className="exercices_nav_item">
-          <NavLink to="/">Equipment</NavLink>
-        </li>
-      </ul>
-    </div>
-  ) 
+  return <div></div> 
 };
 
 export default ExercisesCategories;
--- src/pages/Exercises.jsx
+++ src/pages/Exercises.jsx
@@ -1,9 +1,23 @@
 import SectionTemplate from '../components/SectionTemplate/SectionTemplate';
+import {NavLink} from 'react-router-dom';
 
 const Exercises = () => {
   return (
     <SectionTemplate>
-      <p>Exercises page</p>
+      <div className='ExercisesCategories_ExercisesPage'>
+        <h2>Exercices</h2>
+        <ul className="exercices_nav">
+          <li className="exercices_nav_item">
+            <NavLink to="/">Body parts</NavLink>
+          </li>
+          <li className="exercices_nav_item">
+            <NavLink to="/">Muscles</NavLink>
+          </li>
+          <li className="exercices_nav_item">
+            <NavLink to="/">Equipment</NavLink>
+          </li>
+        </ul>
+      </div>
     </SectionTemplate>
   );
 };
