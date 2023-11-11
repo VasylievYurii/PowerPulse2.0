8b1f08b2e8447efcdb685d202d4ee9220431f5a0
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-11-11 17:13:53 +0200

    add Container,   Left,   Content,   Title,   Text,   Svg,   StyledLink,

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,18 +1,52 @@
-import { Container, Title, Text, LinkBtn, Button } from './ErrorPage.styled';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+  Container,
+  Left,
+  Content,
+  Title,
+  Text,
+  Svg,
+  StyledLink, 
+} from './ErrorPage.styled';
+import  Button  from '../../components/LogOutBtn/LogOutBtn';
+
+/* import svg */
+
 
 const ErrorPage = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/');
+  };
+
   return (
     <Container>
-      <Title>404</Title>
-      <Text>Sorry, you have reached a page that we could not find. It seems that you are lost among
-        the numbers and letters of our virtual space. Perhaps this page went on vacation or
-        decided to disappear into another dimension. We apologize for this inconvenience.</Text>
-      <LinkBtn to="/diary">
-        <Button>Go Home</Button>
-      </LinkBtn>
+      <StyledLink to="/">
+        {/*  <Svg>
+          <use href={`$`} />
+        </Svg> */}
+      </StyledLink>
+      <Left>
+        <Content>
+          <Title>404</Title>
+          <Text>
+            Sorry, you have reached a page that we could not find. It seems
+            that you are lost among the numbers and letters of our virtual
+            space. Perhaps this page went on vacation or decided to disappear
+            into another dimension. We apologize for this inconvenience.
+          </Text>
+             <Button
+            className="Button"
+            text="Go Home"
+            type="button"
+            onClick={handleClick}
+          />
+        </Content>
+      </Left>
     </Container>
   );
 };
 
 export default ErrorPage;
-
