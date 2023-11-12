d0444455b2f53a80ac45c6f7589996c4273069ee
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-11-12 14:23:40 +0200

    Update AddExerciseForm.jsx

--- src/components/AddExerciseForm/AddExerciseForm.jsx
+++ src/components/AddExerciseForm/AddExerciseForm.jsx
@@ -1,7 +1,55 @@
 import React from 'react';
+import {Container, LeftContainer, GifWrapper, Timer, CountdownCircleTimer, Calories, RightContainer, List, ListItem, ButtonContainer} from './AddExerciseForm.styled';
 
 const AddExerciseForm = () => {
-  return <div></div>;
-};
+
+    return (
+      <Container>
+        <LeftContainer>
+          <GifWrapper>
+            <Gif src={gifUrl} alt={name} />
+          </GifWrapper>
+          <Timer>
+            <TimerTitle>Time</TimerTitle>
+            <TimerWrapper>
+              <CountdownCircleTimer >
+                 {/* renderTime */}
+              </CountdownCircleTimer>
+            </TimerWrapper>
+            <TimerButton type="button" onClick={toggleIsPlaying}>
+             {/*  <svg className="timerButtonIcon">
+                <use href={isPlaying ? `${sprite}` : `${sprite}`} />
+              </svg> */}
+            </TimerButton>
+            <Calories>
+              Burned calories: <span className="caloriesSpan">{calculatedCalories}</span>
+            </Calories>
+          </Timer>
+        </LeftContainer>
+        <RightContainer>
+          <List>
+            <ListItem>
+              <ItemTitle>Name</ItemTitle>
+            </ListItem>
+            <ListItem>
+              <ItemTitle>Target</ItemTitle>
+            </ListItem>
+            <ListItem>
+              <ItemTitle>Body Part</ItemTitle>
+            </ListItem>
+            <ListItem>
+              <ItemTitle>Equipment</ItemTitle>
+            </ListItem>
+          </List>
+          <ButtonContainer>
+            <Button title="Add to diary" onClick={handleAddToDiary} />
+          </ButtonContainer>
+        </RightContainer>
+      </Container>
+    );
+  };
 
 export default AddExerciseForm;
+
+
+
