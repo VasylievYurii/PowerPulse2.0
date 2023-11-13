32a9ceff1028302817cfb5afb3bce7bc6839e754
VasylievYurii <uraplanetianin@gmail.com>
2023-11-13 05:14:27 +0200

    diary get and map

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -4,27 +4,24 @@ import SectionTemplate from '../components/SectionTemplate/SectionTemplate';
 import TitlePage from '../components/TitlePage';
 import { getDiaryMealsThunk } from '../redux/diary/diaryOperations';
 import DiaryCalendar from '../components/DiaryCalendar';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { format } from 'date-fns';
 import { DiaryWrapTitle } from './Diary/Diary.styled';
 
 const Diary = () => {
   const dispatch = useDispatch();
 
-  const getMeal = (diaryData) => {
-    dispatch(getDiaryMealsThunk(diaryData));
-  };
-
   const [selectedDate, setSelectedDate] = useState(
-    format(new Date(), "yyyy-MM-dd"),
+    format(new Date(), 'yyyy-MM-dd'),
   );
-
-  let newDate = format(new Date(selectedDate), "yyyy-MM-dd");
-  console.log('selectedDate:', newDate);
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
+
+  useEffect(() => {
+    dispatch(getDiaryMealsThunk(selectedDate));
+  }, [selectedDate]);
 
   return (
     <SectionTemplate>
@@ -32,15 +29,6 @@ const Diary = () => {
         <TitlePage>Diary</TitlePage>
         <DiaryCalendar onDateChange={handleDateChange} />
       </DiaryWrapTitle>
-      
-      <button
-        type="button"
-        onClick={() => getMeal({data: {"date": "2023-11-12"}})}
-        // onClick={() => getMeal(selectedDate)}
-      >
-        GetMeal
-      </button>
-
       <DayProducts />
     </SectionTemplate>
   );
--- src/redux/diary/diaryOperations.js
+++ src/redux/diary/diaryOperations.js
@@ -1,71 +1,56 @@
-import axios from "axios";
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { instance } from "../auth/operations";
+import { instance } from '../auth/operations';
 
 const options = {
-    position: "top-right",
-    autoClose: 4000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "dark",
+  position: 'top-right',
+  autoClose: 4000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: 'dark',
 };
 
 const toastError = (text) => {
-    toast.error(text, options);
+  toast.error(text, options);
 };
 
 const toastSuccess = (text) => {
-    toast.success(text, options);
+  toast.success(text, options);
 };
 
 const getMeals = async (diaryData, thunkAPI) => {
-    try {
-        console.log('diaryData ->', diaryData);
-        // const searchData = {
-        //     date: diaryData
-        // };
-        // console.log('searchData ->', searchData);
+  try {
+    const response = await instance.get(`dairys/meals/${diaryData}`);
 
-        const response = await instance.get('dairys/meals', diaryData);
-
-        console.log(response);
-return response.data;
-    }
-    catch (e) {
-        console.log(e.message);
-toastError(`Oops! Something was wrong.... ${e.message}`);
-        return thunkAPI.rejectWithValue(e.message);
-    }
+    console.log(response);
+    return response.data;
+  } catch (e) {
+    console.log(e.message);
+    toastError(`Oops! Something was wrong.... ${e.message}`);
+    return thunkAPI.rejectWithValue(e.message);
+  }
 };
 
 const delMeal = async (mealId, thunkAPI) => {
-    try {
-        console.log(mealId);
-                const response = await instance.delete(`dairys/meals`, mealId);
+  try {
+    console.log(mealId);
+    const response = await instance.delete(`dairys/meals/${mealId}`);
 
-        toastSuccess(`Meal delete successfully`);
-        console.log(response);
-return response.data;
-    }
-    catch (e) {
-        console.log(e.message);
-            toastError(`Oops! Something was wrong.... ${e.message}`);
-        return thunkAPI.rejectWithValue(e.message);
-    }
+    toastSuccess(`Meal delete successfully`);
+    console.log(response);
+    return response.data;
+  } catch (e) {
+    console.log(e.message);
+    toastError(`Oops! Something was wrong.... ${e.message}`);
+    return thunkAPI.rejectWithValue(e.message);
+  }
 };
 
+export const getDiaryMealsThunk = createAsyncThunk('diary/getMeals', getMeals);
 
-export const getDiaryMealsThunk = createAsyncThunk(
-    'diary/getMeals',
-    getMeals
-);
-
-export const delDiaryMealsThunk = createAsyncThunk(
-    'diary/delMeal',
-    delMeal
-);
+export const delDiaryMealsThunk = createAsyncThunk('diary/delMeal', delMeal);
