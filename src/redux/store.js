1a231847b3997198c1d58f9c40c82c61eefca6cb
KaterynaValuiska <katya.barry.ubf@gmail.com>
2023-11-13 09:58:56 +0100

    avatars

--- src/redux/store.js
+++ src/redux/store.js
@@ -14,6 +14,7 @@ import {
 import storage from 'redux-persist/lib/storage';
 import { authReducer } from './auth/authSlice';
 import { diaryReducer } from './diary/diarySlice';
+import { userReducer } from './user/userSlice';
 // import { contactsReducer } from './contacts/contactsSlice';
 // import { filterReducer } from './contacts/filterSlice';
 
@@ -27,6 +28,7 @@ const combinedReducers = combineReducers({
   //   contacts: contactsReducer,
   //   filter: filterReducer,
   diary: diaryReducer,
+  users: userReducer,
   auth: persistReducer(authPersistConfig, authReducer),
 });
 
