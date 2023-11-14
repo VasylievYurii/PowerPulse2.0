f0ab81f192eeb1899a56ac3da3421da255fe4e7b
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-11-14 18:28:47 +0200

    Update AddExerciseForm.jsx

--- src/components/AddExerciseForm/AddExerciseForm.jsx
+++ src/components/AddExerciseForm/AddExerciseForm.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import ExersiceModalWindowList from './ExersiceModalWindowList/ExersiceModalWindowList';
+
 import {
   ExersiceModalWindowWrap,
   ExersiceModalContainer,
@@ -9,41 +10,77 @@ import {
   ExersiceModalTimer,
 } from './AddExerciseForm.styled';
 
+/* import { getUserParams } from '../../redux/auth/operations'; */
+/* import { useEffect } from '../../redux/auth/operations';
+import { useDispatch } from 'react-redux';
+import { toast } from 'react-toastify'; */
+ /* import { addExercise } from '../../redux/diary/diaryOperations'; */
+ 
 const formatDate = date => {
   const day = String(date.getDate()).padStart(2, '0');
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const year = date.getFullYear();
   return `${day}/${month}/${year}`;
-}
-
-export const ExersiceModalWindow = () => {
-  const { bodyPart, equipment, burnedCalories, gifUrl, name, target, _id, time } = data;
-}
-
-const formattedDate = formatDate(date);
-
-const handleAddToDiary = () => {
-  if (!amount) {
-    toast.error('Must be greater than 0');
-    return;
-  }
-
-  dispatch(
-    addExercise({
-      date: formattedDate, 
-      bodyPart,
-      target,
-      time: dinamicTime,
-      exerciseId: _id,
-      equipment,
-      name,
-      burnedCalories: dinamicBurnCal,
-    }),
-  );
-  onClick();
 };
 
-const AddExerciseForm = () => {
+export const ExersiceModalWindow = ({ data, onClick, closeModal }) => {
+  const {
+    bodyPart,
+    equipment,
+    burnedCalories,
+    gifUrl,
+    name,
+    target,
+    _id,
+    time,
+  } = data;
+
+  const [dinamicBurnCal, setDinamicBurnCal] = useState(0);
+  const [dinamicTime, setDinamicTime] = useState(0);
+
+  const dispatch = useDispatch();
+  
+
+  useEffect(() => {
+    dispatch(getUserParams());
+  }, [dispatch]);
+
+  const amount = Math.round((burnedCalories / (time * 60)) * 180);
+  // ;
+
+  const savedDate = localStorage.getItem('selectedDate');
+  let date = new Date(); // Default to current date
+
+  if (savedDate) {
+    const parsedDate = new Date(savedDate);
+    if (!isNaN(parsedDate.getTime())) {
+      date = parsedDate; // Use parsed date if valid
+    }
+  }
+
+  const formattedDate = formatDate(date);
+
+  const handleAddToDiary = () => {
+    if (!amount) {
+      toast.error('Must be greater than 0');
+      return;
+    }
+
+    dispatch(
+      addExercise({
+        date: formattedDate, // Use the formatted date
+        bodyPart,
+        target,
+        time: dinamicTime,
+        exerciseId: _id,
+        equipment,
+        name,
+        burnedCalories: dinamicBurnCal,
+      }),
+    );
+    onClick();
+    // closeModal();
+  };
 
   return (
     <ExersiceModalContainer>
@@ -59,13 +96,13 @@ const AddExerciseForm = () => {
             setDinamicTime={setDinamicTime}
           /> */}
         </ExersiceModalTimer>
-       {/*  <ExersiceModalWindowList
+        <ExersiceModalWindowList
           name={name}
           bodypart={bodyPart}
           target={target}
           equipment={equipment}
           time={time}
-        /> */}
+        />
         <BoxBtn>
           <ExersiceModalWindowBtn type="button" onClick={handleAddToDiary}>
             Add to diary
@@ -73,10 +110,5 @@ const AddExerciseForm = () => {
         </BoxBtn>
       </ExersiceModalWindowWrap>
     </ExersiceModalContainer>
-  )
-  };
-
-export default AddExerciseForm;
-
-
-
+  );
+};
