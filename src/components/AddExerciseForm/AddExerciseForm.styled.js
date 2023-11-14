d61f8fbe0ab27d4c7fbd2424c3209cd78a3ca8c4
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-11-14 02:32:22 +0200

    Update AddExerciseForm.styled.js

--- src/components/AddExerciseForm/AddExerciseForm.styled.js
+++ src/components/AddExerciseForm/AddExerciseForm.styled.js
@@ -18,11 +18,13 @@ max-width: 344px;
     flex-direction: column;
 
 ;` 
-/* export const GifWrapper = styled.div`
+ export const GifWrapper = styled.div`
   width: 270px;
   height: 226px;
   margin-left: auto;
   margin-right: auto;
+  background-color: blue;
+  margin-bottom: 14px;
 
   @media screen and (min-width: 768px) {
     margin-left: 32px;
@@ -32,7 +34,7 @@ max-width: 344px;
 `;
 
 
-export const Gif = styled.img`
+/*export const Gif = styled.img`
 width: 270px;
 height: 226px;
 border-radius: 12px;
@@ -45,27 +47,74 @@ padding: 105px, 45px;
   }
 ;`
  */
+export const TitleWrapper = styled.div`
+display: flex;
+  flex-direction: column;
+  align-items: center;
+;`
 export const TimerTitle = styled.p`
 font-size: 10px;
-height: 14px;
+height: 1.4;
+margin-bottom: 4px;
 color: var(--color-text);
 ;`
 
+export const TimerWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+margin-bottom: 14px;
+;`
+
 export const CountdownCircleTimer = styled.p`
-font-size: 10px;
+width: 125px;
+height: 124px;
+background-color: blue;
+border-radius:100px;
 ` ;
+
+export const TimerButtonWrapper = styled.div`
+ @media screen and (min-width: 768px) {
+    grid-area: button;
+    display: flex;
+    justify-content: center;
+
+    @media screen and (min-width: 768px) {
+      margin-right: 32px;
+      margin-top: auto;
+      margin-bottom: 48px;
+    }
+  }
+;`
 
 export const TimerButton = styled.button`
     width: 32px;
     height: 32px;
+    border-radius: 10px;
+    padding: 0px;
+  margin-bottom: 8px;
+  cursor: pointer;
     background: var(--color-main-one);
     color: var(--color-text);
 `; 
+/* export const IconTimer = styled.svg`
+width: 6px;
+height: 8px;
+fill: var(--color-text);
+
+;` */
+/* 
+export const CaloriesWrapper = styled.div`
+display: flex;
+  flex-direction: column;
+  align-items: center;
+;` */
 
 export const Calories = styled.h2`
 font-size: 14px;
 line-height: 1.3;
 color: var(--color-text);
+
 ;`
 
 export const RightContainer = styled.div`
