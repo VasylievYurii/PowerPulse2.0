4c90e666eb3e7f9a7cabc19b66ba6ce9af9ae6e2
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-11-14 17:15:36 +0200

    ExersiceModalWindowList.styled

--- src/components/AddExerciseForm/ExersiceModalWindowList/ExersiceModalWindowList.jsx
+++ src/components/AddExerciseForm/ExersiceModalWindowList/ExersiceModalWindowList.jsx
@@ -1,10 +1,31 @@
-import { ExersiceModalList } from "./ExersiceModalWindowList.styled"
+import { ExersiceModalList, ExersiceModalLink, ExersiceModalLinkSub, ExersiceModalLinkTitle } from "./ExersiceModalWindowList.styled"
 
-const ExersiceModalWindowList = () => {
-
+const ExersiceModalWindowList = ({name, bodypart, target, equipment, time}) => {
+  const firstLetterToUppercase = (title) => {
+    return title.replace(title[0], title[0].toUperCase());
+}
     return (
         <ExersiceModalList>
-
+<ExersiceModalLink>
+        <ExersiceModalLinkSub >Name</ExersiceModalLinkSub>
+        <ExersiceModalLinkTitle >{firstLetterToUppercase(name)}</ExersiceModalLinkTitle>
+      </ExersiceModalLink>
+      <ExersiceModalLink >
+        <ExersiceModalLinkSub >Target</ExersiceModalLinkSub>
+        <ExersiceModalLinkTitle >{firstLetterToUppercase(target)}</ExersiceModalLinkTitle>
+      </ExersiceModalLink>
+      <ExersiceModalLink >
+        <ExersiceModalLinkSub >Body Part</ExersiceModalLinkSub>
+        <ExersiceModalLinkTitle >{firstLetterToUppercase(bodypart)}</ExersiceModalLinkTitle>
+      </ExersiceModalLink>
+      <ExersiceModalLink >
+        <ExersiceModalLinkSub >Equipment</ExersiceModalLinkSub>
+        <ExersiceModalLinkTitle>{firstLetterToUppercase(equipment)}</ExersiceModalLinkTitle>
+      </ExersiceModalLink>
+      <ExersiceModalLink >
+        <ExersiceModalLinkSub >Time</ExersiceModalLinkSub>
+        <ExersiceModalLinkTitle>{time} minutes</ExersiceModalLinkTitle>
+      </ExersiceModalLink>
         </ExersiceModalList>
 
 
