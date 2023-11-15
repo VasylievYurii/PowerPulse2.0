7e0f753d3cc02ad2b206dfde28fb99591f9313a8
VasylievYurii <122171281+VasylievYurii@users.noreply.github.com>
2023-11-15 21:38:12 +0200

    Revert "commit"

--- src/redux/products/productsOperations.js
+++ src/redux/products/productsOperations.js
@@ -4,31 +4,19 @@ import { instance } from '../auth/operations';
 
 // axios.defaults.baseURL = 'https://powerpulse-t5-backend.onrender.com/api';
 
-export const getProducts = createAsyncThunk(
-  'products/getProducts',
-  async (
-    { recommended = '', categoryId = '', query = '', page, limit },
-    thunkAPI,
-  ) => {
-    try {
-      const { data } = await instance.get(
-        `products?recommended=${recommended}&categoryId=${categoryId}&query=${query}&page=${page}&limit=${limit}`,
-      );
-      console.log('data getProducts', data);
-      return data;
-    } catch (e) {
-      console.log(e.message);
-      return thunkAPI.rejectWithValue(e.message);
-    }
-  },
-);
-
-export const getProductsCategories = createAsyncThunk(
-  'products/getCategories',
+export const getProductsThunk = createAsyncThunk(
+  'filters/getProducts',
   async (_, thunkAPI) => {
     try {
-      const { data } = await instance.get(`products/categories`);
-      // console.log('categories', data);
+      const persistedToken =
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NTJhMjlhYzlkMzg0MGM4NGVmMzVhMSIsImNyZWF0ZWRBdCI6IjIwMjMtMTEtMTNUMjI6MjY6MzQuODkyWiIsImlhdCI6MTY5OTk5OTc3NSwiZXhwIjoxNzAwMTA3Nzc1fQ.6tgnK-H4OUlggC3x_cD0eTcVRf3uj3UvgfFe-CBKVMk';
+      console.log('persistedToken', persistedToken);
+
+      // Встановлення токену в хедери
+      instance.defaults.headers['Authorization'] = `Bearer ${persistedToken}`;
+
+      const { data } = await instance.get(`/products`);
+      console.log('data', data);
       return data;
     } catch (e) {
       console.log(e.message);
--- src/redux/products/productsSlice.js
+++ src/redux/products/productsSlice.js
@@ -1,5 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { getProducts, getProductsCategories } from './productsOperations';
+import {
+  getProductsThunk,
+  // fetchProductsByCategories,
+} from './productsOperations';
 
 const initialProductsState = {
   products: [], // перші 15 продуктів
@@ -9,41 +12,39 @@ const initialProductsState = {
 };
 
 const productsFilterSlice = createSlice({
-  name: 'products',
+  name: 'filters',
   initialState: initialProductsState,
 
   extraReducers: (builder) => {
     builder
-      .addCase(getProducts.fulfilled, (state, action) => {
+      .addCase(getProductsThunk.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
         state.products = action.payload;
       })
-      .addCase(getProducts.pending, (state) => {
+      .addCase(getProductsThunk.pending, (state) => {
         state.isLoading = true;
         state.error = null;
       })
-      .addCase(getProducts.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-      })
-      .addCase(getProductsCategories.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.error = null;
-        // Оновити поле categories у стані
-        state.categories = action.payload;
-        // state.categories = action.payload[0].items;
-      })
-      .addCase(getProductsCategories.pending, (state) => {
-        state.isLoading = true;
-        state.error = null;
-      })
-      .addCase(getProductsCategories.rejected, (state, action) => {
+      .addCase(getProductsThunk.rejected, (state, action) => {
         state.isLoading = false;
         state.error = action.payload;
       });
+    // .addCase(fetchProductsByCategories.fulfilled, (state, action) => {
+    //   state.isLoading = false;
+    //   state.error = null;
+    //   state.categories = action.payload;
+    // })
+    // .addCase(fetchProductsByCategories.pending, (state) => {
+    //   state.isLoading = true;
+    //   state.error = null;
+    // })
+    // .addCase(fetchProductsByCategories.rejected, (state, action) => {
+    //   state.isLoading = false;
+    //   state.error = action.payload;
+    // });
   },
 });
 
 export const productsReducer = productsFilterSlice.reducer;
-export const filterReducer = productsFilterSlice.actions;
+export const productReducer = productsFilterSlice.actions.setFilterProducts;
