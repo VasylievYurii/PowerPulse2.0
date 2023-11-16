517bc2c7f6523581699b08bac54b6fc53742f330
VasylievYurii <uraplanetianin@gmail.com>
2023-11-17 00:20:38 +0200

    The code from Katya`s fork

--- src/components/ExercisesList/ExercisesList.jsx
+++ src/components/ExercisesList/ExercisesList.jsx
@@ -1,30 +1,50 @@
-import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import ExercisesItem from '../ExercisesItem/ExercisesItem';
+import { useParams } from 'react-router-dom';
+import { getExercisesFilter } from '../../redux/exercises/exeOperation';
+import {
+  NameExersises,
+  GeneralWraper,
+  WrapperExercises,
+  ButtonGoBack,
+  LinkBtn,
+  IconWraperBack,
+} from './ExercisesList.styled';
+import sprite from '../../assets/sprite.svg';
 
 const ExercisesList = () => {
-  const [exersises, setExersises] = useState([]);
-  const { array } = useSelector((state) => state.exercises);
+  const location = useLocation();
+  const backLinkLocation = useRef(location.state?.from ?? '/exercises');
+  const dispatch = useDispatch();
+  // const [exersises, setExersises] = useState([]);
+  const { exeFilter } = useSelector((state) => state.exercises);
+  const params = useParams();
+  const current = params.id;
   useEffect(() => {
-    if (array) {
-      const exersisesPart = arraybodyPart(array, 'chest');
-      setExersises(exersisesPart);
+    const paramsExe = {
+      filter: params.filter,
+      name: params.id,
+    };
+    if (paramsExe) {
+      dispatch(getExercisesFilter(paramsExe));
     }
-  }, [array]);
-  function arraybodyPart(arr, text) {
-    const arrayExe = arr.filter((item) => item.bodyPart === text);
-    console.log(arrayExe);
-    return arrayExe;
-  }
-  return (
+  }, [dispatch]);
+  console.log('tut', exeFilter);
+  const ucFirst = (str) => {
+    if (!str) return str;
+    return str[0].toUpperCase() + str.slice(1);
+  };
 
+  return (
     <GeneralWraper>
       <div>
         <ButtonGoBack>
           <IconWraperBack>
-            <use href={`${sprite}#icon-next`} width={24} height={24} />
+            <use href={`${sprite}#con-next`} width={24} height={24} />
+            <LinkBtn to={backLinkLocation.current}>Back</LinkBtn>
           </IconWraperBack>
-          <LinkBtn to={backLinkLocation.current}>Back</LinkBtn>
         </ButtonGoBack>
         <NameExersises>{ucFirst(current)}</NameExersises>
         <WrapperExercises>
@@ -40,7 +60,6 @@ const ExercisesList = () => {
         </WrapperExercises>
       </div>
     </GeneralWraper>
- 
   );
 };
 
