b5ecf5ac0542fc752300f1a1454ec7a2d3c40107
VasylievYurii <uraplanetianin@gmail.com>
2023-11-18 04:01:46 +0200

    calendar for Diary Page

--- src/components/DiaryCalendar/DiaryCalendar.jsx
+++ src/components/DiaryCalendar/DiaryCalendar.jsx
@@ -1,15 +1,17 @@
 import { useState, forwardRef } from 'react';
 import DatePicker from 'react-datepicker';
-import { format } from 'date-fns';
+import { format, subDays, addDays } from 'date-fns';
 import {
   CalendarGlobalStyles,
   TitleWrapper,
   CalendarWrapper,
   IconWrapper,
   IconCalendar,
+  CustomHeaderWrapper,
+  IconWrapperHeader,
+  HeaderData,
 } from './DiaryCalendar.styled';
 import 'react-datepicker/dist/react-datepicker.css';
-// import '../../../node_modules/react-datepicker/dist/react-datepicker.css';
 import sprite from '../../assets/sprite.svg';
 
 const DiaryCalendar = ({ onDateChange }) => {
@@ -20,23 +22,39 @@ const DiaryCalendar = ({ onDateChange }) => {
       <CalendarWrapper>
         <TitleWrapper onClick={onClick} ref={ref}>
           {format(startDate, 'dd/MM/yyyy')}
+          <IconCalendar onClick={onClick} ref={ref}>
+            <use href={`${sprite}#icon-calendar`} />
+          </IconCalendar>
         </TitleWrapper>
-        <IconCalendar onClick={onClick} ref={ref}>
-          <use href={`${sprite}#icon-calendar`} />
-        </IconCalendar>
-        <IconWrapper>
+
+        <IconWrapper onClick={() => setStartDate(subDays(startDate, 1))}>
           <use href={`${sprite}#icon-left`} />
         </IconWrapper>
-        <IconWrapper>
+        <IconWrapper onClick={() => setStartDate(addDays(startDate, 1))}>
           <use href={`${sprite}#icon-right`} />
         </IconWrapper>
       </CalendarWrapper>
     );
   });
 
+  const CustomHeader = ({ date, decreaseMonth, increaseMonth }) => {
+    return (
+      <CustomHeaderWrapper>
+        <IconWrapperHeader onClick={decreaseMonth}>
+          <use href={`${sprite}#icon-left`} />
+        </IconWrapperHeader>
+        <HeaderData>{format(date, 'MMMM yyyy')}</HeaderData>
+        <IconWrapperHeader onClick={increaseMonth}>
+          <use href={`${sprite}#icon-right`} />
+        </IconWrapperHeader>
+      </CustomHeaderWrapper>
+    );
+  };
+
   return (
     <>
       <DatePicker
+        minDate={new Date()}
         selected={startDate}
         onChange={(date) => {
           setStartDate(date);
@@ -46,7 +64,7 @@ const DiaryCalendar = ({ onDateChange }) => {
         dateFormat={'dd MM yyyy'}
         calendarStartDay={1}
         formatWeekDay={(day) => day.substr(0, 2)}
-        className="my-cal"
+        renderCustomHeader={(props) => <CustomHeader {...props} />}
       />
       <CalendarGlobalStyles />
     </>
